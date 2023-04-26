26e08d34f99ba03ebdb0e2a991a63d94662f43b2
Tina Dam <124227045+dtsunai@users.noreply.github.com>
2023-04-26 16:10:57 -0700

    file_name_exercise2
    Edits: individual functions for each list task/variation

--- file_name_exercise.py
+++ file_name_exercise.py
@@ -5,134 +5,138 @@
     
     # >>> fnp = file name pure (without extension)
 
-def myfunc():
-    list_of_files = ["Anderson_Kevin.pdf", "Williams_Samantha.pdf", "Matthews_Jane.pdf", "Dean_Andrew.pdf", "Laris_Cody.pdf"]
-    kander, swilli, jmatth, adean, claris = list_of_files
+list_of_files = ["Anderson_Kevin.pdf", "Williams_Samantha.pdf", "Matthews_Jane.pdf", "Dean_Andrew.pdf", "Laris_Cody.pdf"]
+kander, swilli, jmatth, adean, claris = list_of_files
     
-    underSymbol = "_"
-    dotSymbol = "."
-    commaSymbol = ","
-    space = " "
-    
-    print(type(list_of_files))
-    print(type(list_of_files[0]))
-    print(list_of_files[0][-4:])
+underSymbol = "_"
+dotSymbol = "."
+commaSymbol = ","
+space = " "
+
+
+#finding length of file name
+def findLength(x):
+    fn_length = len(x)
+    dotPosition = x.find(dotSymbol)
+    fnp_length = len(x[:dotPosition])  
+    fnp = x[:fnp_length]
+
+    print(x)
+    print("Length of name, excluding extension:",fnp_length)
     print()
-    
-    #finding length of file name
-    for x in list_of_files:
-        fn_length = len(x)
+
+#checking format if _ is present   
+def checkFormat(x):
+    print(x)
         
-        print(x)
-        print("Length of name, excluding extension:",(fn_length - 4))
+    if underSymbol in x:
+        print("The above file is correct")
         print()
-    
-    #checking format if _ is present
-    for x in list_of_files:
-        print(x)
         
-        if underSymbol in x:
-            print("The above file is correct")
-            print()
-    
-    #displaying file name without extension
-    print()
-    print("File Name Without Extension")
-    print("---------------------------------")   
-    for x in list_of_files:
-    
-        dotPosition = x.find(".")
+#cleaning file name to exclude extension    
+def makePure(x):
+        dotPosition = x.find(dotSymbol)
         fnp_length = len(x[:dotPosition])  
         fnp = x[:fnp_length]
         
-        print(fnp)
-        
-        #fn_name = x[:-4]
-        #print(fn_name)
-    
-    #displaying last name, first name
-    print()
-    print("Last Name, First Name")
-    print("--------------------------")           
-    for x in list_of_files:
-        
-        dotPosition = x.find(".")
+        print(fnp) 
+
+#giving lastName, firstName
+def giveLastFirstName(x):
+        dotPosition = x.find(dotSymbol)
         fnp_length = len(x[:dotPosition])  
         fnp = x[:fnp_length]
         
         if underSymbol in fnp:
             lastFirstName = fnp.replace(underSymbol, commaSymbol + space)
             print(lastFirstName)
+
+#giving last name from file name
+def giveLastName(x):
+        dotPosition = x.find(dotSymbol)
+        fnp_length = len(x[:dotPosition])  
+        fnp = x[:fnp_length]
+        
+        underPosition = fnp.find(underSymbol)     
+        lastName = fnp[:underPosition]
+        
+        print(lastName)
+
+#giving first name from file name        
+def giveFirstName(x):
+        dotPosition = x.find(dotSymbol)
+        fnp_length = len(x[:dotPosition])  
+        fnp = x[:fnp_length]
+        
+        underPosition = fnp.find(underSymbol)
+        firstName = fnp[underPosition + 1:]
+        
+        print(firstName)
+        
+def giveFullName(x):
+        dotPosition = x.find(dotSymbol)
+        fnp_length = len(x[:dotPosition])  
+        fnp = x[:fnp_length]
+        
+        underPosition = fnp.find(underSymbol)     
+        lastName = fnp[:underPosition]
+        
+        underPosition = fnp.find(underSymbol)
+        firstName = fnp[underPosition + 1:]
+        
+        fullName = firstName + space + lastName
+        print(fullName)
+        
+def myfunc():
     
-        #y = x[:-4]
-        #if "_" in y:
-            #z = y.replace("_", ", ")
-            #print(z)
+    print(type(list_of_files))
+    print(type(list_of_files[0]))
+    
+    dotPosition = list_of_files[0].find(dotSymbol)
+    print(list_of_files[0][dotPosition:])
+    print()
+    
+    #displaying length of file name
+    for x in list_of_files:
+        findLength(x)
+    
+    #displaying if format is correct
+    for x in list_of_files:
+        checkFormat(x)
+    
+    #displaying file name without extension
+    print()
+    print("File Name Without Extension")
+    print("---------------------------------")   
+    for x in list_of_files:
+        makePure(x)
+        
+    #displaying last name, first name
+    print()
+    print("Last Name, First Name")
+    print("--------------------------")           
+    for x in list_of_files:
+        giveLastFirstName(x)
     
     #displaying last name only
     print()
     print("Last Name")
     print("------------")
     for x in list_of_files:
-    
-        dotPosition = x.find(".")
-        fnp_length = len(x[:dotPosition])  
-        fnp = x[:fnp_length]
-        
-        underPosition = fnp.find("_")     
-        lastName = fnp[:underPosition]
-        
-        print(lastName)
-        
-        #y = x[:-4]
-        #z = y.find("_")
-        
-        #print(y[:z])
+        giveLastName(x)
     
     #displaying first name only
     print()
     print("First Name")
     print("------------")
     for x in list_of_files:
-        
-        dotPosition = x.find(".")
-        fnp_length = len(x[:dotPosition])  
-        fnp = x[:fnp_length]
-        
-        underPosition = fnp.find("_")
-        firstName = fnp[underPosition + 1:]
-        
-        print(firstName)
-        
-        #y = x[:-4]
-        #z = y.find("_") + 1
-        
-        #print(y[z:])
+        giveFirstName(x)
     
     #displaying first name last name
     print()
     print("Full Name")
     print("------------")
     for x in list_of_files:
-        
-        dotPosition = x.find(".")
-        fnp_length = len(x[:dotPosition])  
-        fnp = x[:fnp_length]
-        
-        underPosition = fnp.find("_")     
-        lastName = fnp[:underPosition]
-        
-        underPosition = fnp.find("_")
-        firstName = fnp[underPosition + 1:]
-        
-        fullName = firstName + space + lastName
-        print(fullName)
-        
-        
-        #y = x[:-4]
-        #z = y.find("_")
-        #w = z + 1
-        
-        #print(y[w:], y[:z])
+        giveFullName(x)
      
 myfunc()
